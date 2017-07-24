003a3300c658daf5113cf48b83ab8b92cd516e02
cowarder <1615482151@qq.com>
2017-07-25 01:14:09 +0800

    selenium

---
+++ selenium.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver=webdriver.PhantomJS()
+
+#获取网址
+driver.get("https://www.douban.com/accounts/login")
+driver.save_screenshot("D:/douban.png")
+
+#输入账号密码
+driver.find_element_by_name("form_email").send_keys("123456")
+driver.find_element_by_name("form_password").send_keys("666666")
+
+#模拟登陆
+driver.find_element_by_name("login").click()
+
+driver.save_screenshot("D:/login.png")
+
